bac938f946dcd2876cf97692c0b27f85b93df6ba
lianwenbo <lianwenbo@douban.com>
2017-05-31 16:07:51 +0800

    fix: Assignment Task finished

--- angularjs/node-examples/node-express-gen/node-mongoose/models/dishes.js
+++ angularjs/node-examples/node-express-gen/node-mongoose/models/dishes.js
@@ -1,6 +1,8 @@
 // grab the things we need
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
 
 var commentSchema = new Schema({
     rating:  {
@@ -25,8 +27,23 @@ var commentSchema = new Schema({
 var dishSchema = new Schema({
     name: {
         type: String,
-        required: true,
         unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    label:{
+        type: String,
+        default: ""
+    },
+    price: {
+        type: Currency,
+        required: true
     },
     description: {
         type: String,
--- angularjs/node-examples/node-express-gen/node-mongoose/server-test.js
+++ angularjs/node-examples/node-express-gen/node-mongoose/server-test.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose'),
     assert = require('assert');
 
-var Dishes = require('./models/dishes-3');
+var Dishes = require('./models/dishes');
+var Promotions = require('./models/promotions.js');
+var Leadership = require('./models/leadership.js');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/conFusion';mongoose.connect(url);
+var url = 'mongodb://localhost:27017/conFusion';
+mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
@@ -12,9 +15,12 @@ db.once('open', function () {
     console.log("Connected correctly to server");
 
     // create a new dish
-    Dishes.create({
+/*    Dishes.create({
         name: 'Uthapizza',
         description: 'Test',
+        image: "images/uthapizza.png",
+        category: "mains",
+        price: "$4.99",
         comments: [
             {
                 rating: 3,
@@ -48,9 +54,10 @@ db.once('open', function () {
                         comment: 'I\'m getting a sinking feeling!',
                         author: 'Leonardo di Carpaccio'
                     });
+                    dish.price = dish.price * 2
 
                     dish.save(function (err, dish) {
-                        console.log('Updated Comments!');
+                        console.log('Updated Comments and price!');
                         console.log(dish);
 
                         db.collection('dishes').drop(function () {
@@ -60,4 +67,31 @@ db.once('open', function () {
                 });
         }, 3000);
     });
+
+    Promotions.create({
+      name: "Weekend Grand Buffet",
+      image: "images/buffet.png",
+      price: "19.99",
+      description: "Featuring . . ."
+    }, 
+    function(err, promotion){
+        if (err) throw err;
+        console.log('Promotion created!');
+        console.log(promotion);
+    });
+    */
+
+   Leadership.create({
+       name: "Peter Pan",
+      image: "images/alberto.png",
+      designation: "Chief Epicurious Officer",
+      abbr: "CEO",
+      description: "Our CEO, Peter, . . ."
+    }, 
+    function(err, leadership){
+        if (err) throw err;
+        console.log('Leadership created!');
+        console.log(leadership);
+    })
+
 });
