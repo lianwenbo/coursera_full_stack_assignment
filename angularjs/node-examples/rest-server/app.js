af082ca1747e4a8d0b8aa4e98b0d14031ccb8da2
lianwenbo <lianwenbo@douban.com>
2017-06-03 21:31:09 +0800

    fix: rest api for mongoose

--- angularjs/node-examples/rest-server/app.js
+++ angularjs/node-examples/rest-server/app.js
@@ -4,9 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var url = 'mongodb://localhost:27017/conFusion';
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log("Connected correctly to server");
+});
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
 
@@ -24,6 +38,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leadership',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,6 +48,19 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 // error handler
 app.use(function(err, req, res, next) {
--- angularjs/node-examples/rest-server/routes/promoRouter.js
+++ angularjs/node-examples/rest-server/routes/promoRouter.js
@@ -1,5 +1,8 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+var mongoose = require('mongoose');
+
+var Promotions = require('../models/promotions');
 
 
 var promoRouter = express.Router();
@@ -7,41 +10,57 @@ var promoRouter = express.Router();
 promoRouter.use(bodyParser.json());
 
 promoRouter.route('/')
-.all(function(req,res,next) {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      next();
-})
-
 .get(function(req,res,next){
-        res.end('Will send all the promotions to you!');
+        Promotions.find({}, function (err, promotion) {
+                if (err) throw err;
+                res.json(promotion);
+        });
 })
 
 .post(function(req, res, next){
-    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);    
+    Promotions.create(req.body, function (err, promotion) {
+        if (err) throw err;
+        console.log('Promotion created!');
+        var id = promotion._id;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the promotion with id: ' + id);
+    });
 })
 
 .delete(function(req, res, next){
-        res.end('Deleting all promotion');
+        Promotions.remove({}, function (err, resp) {
+                if (err) throw err;
+                res.json(resp);
+        });
 });
 
 promoRouter.route('/:promoId')
-.all(function(req,res,next) {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      next();
-})
-
 .get(function(req,res,next){
-        res.end('Will send details of the promotion: ' + req.params.promoId +' to you!');
+        Promotions.findById(req.params.promoId, function (err, promotion) {
+                if (err) throw err;
+                res.json(promotion);
+        });
 })
 
 .put(function(req, res, next){
-        res.write('Updating the promotion: ' + req.params.promoId + '\n');
-    res.end('Will update the promotion: ' + req.body.name + 
-            ' with details: ' + req.body.description);
+        Promotions.findByIdAndUpdate(req.params.promoId, {
+                $set: req.body
+        }, {
+                new: true
+        }, function (err, promotion) {
+                if (err) throw err;
+                res.json(promotion);
+        });
 })
 
 .delete(function(req, res, next){
-        res.end('Deleting promotion: ' + req.params.promoId);
+        Promotions.findByIdAndRemove(req.params.promoId, function (err, resp) {        
+                if (err) throw err;
+                res.json(resp);
+        });
 });
 
 module.exports = promoRouter
