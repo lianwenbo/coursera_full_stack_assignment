394648fe69502fe08ec56b9ad1cefa2637bf53db
lianwenbo <lianwenbo@douban.com>
2017-06-06 20:34:26 +0800

    fix: support tls

--- angularjs/node-examples/rest-server/app.js
+++ angularjs/node-examples/rest-server/app.js
@@ -82,4 +82,14 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// Secure traffic only
+app.all('*', function(req, res, next){
+    console.log('req start: ',req.secure, req.hostname, req.url, app.get('port'));
+  if (req.secure) {
+    return next();
+  };
+
+ res.redirect('https://'+req.hostname+':'+app.get('secPort')+req.url);
+});
+
 module.exports = app;
